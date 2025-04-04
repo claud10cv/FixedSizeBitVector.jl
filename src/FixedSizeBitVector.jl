fb6a77beeac218666b8499985cff932168d8cca8
Claudio Contardo <claudio.contardo@concordia.ca>
2025-04-04 08:46:58 -0400

    exports copy!

--- src/FixedSizeBitVector.jl
+++ src/FixedSizeBitVector.jl
@@ -24,6 +24,6 @@ module FixedSizeBitVector
     include("bitvector2048.jl")
     include("bitvector4096.jl")
 
-    export issubset, (==), (⊻), (&), (|), set!, reset!, flip!, flip, zero, count_ones
+    export issubset, (==), (⊻), (&), (|), set!, reset!, flip!, flip, zero, count_ones, copy!
     export BitVector8, BitVector16, BitVector32, BitVector64, BitVector128, BitVector256, BitVector512, BitVector1024, BitVector2048, BitVector4096
 end # module FixedSizeBitVector
